caffb9b2967fde6c2e3670cd3ca6229df5b4fae7
shofol <jahananower@gmail.com>
2022-11-24 13:13:50 +0600

    add poster and subtitle

--- app.js
+++ app.js
@@ -1,81 +1,105 @@
-const videoContainer = document.querySelector('.video-container');
-const video = document.querySelector('.video-container video');
+const supportsVideo = !!document.createElement("video").canPlayType;
+if (!supportsVideo) {
+  alert("Your browser does not support html5 video player");
+}
 
-const controlsContainer = document.querySelector('.video-container .controls-container');
+const videoContainer = document.querySelector(".video-container");
+const video = document.querySelector(".video-container video");
 
-const playPauseButton = document.querySelector('.video-container .controls button.play-pause');
-const rewindButton = document.querySelector('.video-container .controls button.rewind');
-const fastForwardButton = document.querySelector('.video-container .controls button.fast-forward');
-const volumeButton = document.querySelector('.video-container .controls button.volume');
-const fullScreenButton = document.querySelector('.video-container .controls button.full-screen');
-const playButton = playPauseButton.querySelector('.playing');
-const pauseButton = playPauseButton.querySelector('.paused');
-const fullVolumeButton = volumeButton.querySelector('.full-volume');
-const mutedButton = volumeButton.querySelector('.muted');
-const maximizeButton = fullScreenButton.querySelector('.maximize');
-const minimizeButton = fullScreenButton.querySelector('.minimize');
+const controlsContainer = document.querySelector(
+  ".video-container .controls-container"
+);
+const backButton = document.querySelector(".back-button");
 
+const playPauseButton = document.querySelector(
+  ".video-container .controls button.play-pause"
+);
+const rewindButton = document.querySelector(
+  ".video-container .controls button.rewind"
+);
+const fastForwardButton = document.querySelector(
+  ".video-container .controls button.fast-forward"
+);
+const volumeButton = document.querySelector(
+  ".video-container .controls button.volume"
+);
+const fullScreenButton = document.querySelector(
+  ".video-container .controls button.full-screen"
+);
+const playButton = playPauseButton.querySelector(".playing");
+const pauseButton = playPauseButton.querySelector(".paused");
+const fullVolumeButton = volumeButton.querySelector(".full-volume");
+const mutedButton = volumeButton.querySelector(".muted");
+const maximizeButton = fullScreenButton.querySelector(".maximize");
+const minimizeButton = fullScreenButton.querySelector(".minimize");
 
-const progressBar = document.querySelector('.video-container .progress-controls .progress-bar');
-const watchedBar = document.querySelector('.video-container .progress-controls .progress-bar .watched-bar');
-const timeLeft = document.querySelector('.video-container .progress-controls .time-remaining');
+const progressBar = document.querySelector(
+  ".video-container .progress-controls .progress-bar"
+);
+const watchedBar = document.querySelector(
+  ".video-container .progress-controls .progress-bar .watched-bar"
+);
+const timeLeft = document.querySelector(
+  ".video-container .progress-controls .time-remaining"
+);
 
 let controlsTimeout;
-controlsContainer.style.opacity = '0';
-watchedBar.style.width = '0px';
-pauseButton.style.display = 'none';
-minimizeButton.style.display = 'none';
+controlsContainer.style.opacity = "0";
+watchedBar.style.width = "0px";
+pauseButton.style.display = "none";
+minimizeButton.style.display = "none";
 
 const displayControls = () => {
-  controlsContainer.style.opacity = '1';
-  document.body.style.cursor = 'initial';
+  controlsContainer.style.opacity = "1";
+  document.body.style.cursor = "initial";
+  backButton.style.opacity = "1";
   if (controlsTimeout) {
     clearTimeout(controlsTimeout);
   }
   controlsTimeout = setTimeout(() => {
-    controlsContainer.style.opacity = '0';
-    document.body.style.cursor = 'none';
+    controlsContainer.style.opacity = "0";
+    document.body.style.cursor = "none";
+    backButton.style.opacity = "0";
+
     removePlaybackControls();
-  }, 5000);
+  }, 25000000);
 };
 
 const playPause = () => {
-  if (video.paused) {
-    // video.play();
-    playButton.style.display = 'none';
-    pauseButton.style.display = '';
+  if (video.paused || video.ended) {
+    video.play();
+    playButton.style.display = "none";
+    pauseButton.style.display = "";
 
-    var playPromise = video.play();
+    // var playPromise = video.play();
 
-
-    // In browsers that don’t yet support this functionality,
-    // playPromise won’t be defined.
-    if (playPromise !== undefined) {
-      playPromise.then(function () {
-        // Automatic playback started!
-      }).catch(function (error) {
-        // Automatic playback failed.
-        // Show a UI element to let the user manually start playback.
-      });
-    }
-
-
+    // // In browsers that don’t yet support this functionality,
+    // // playPromise won’t be defined.
+    // if (playPromise !== undefined) {
+    //   playPromise
+    //     .then(function () {
+    //       // Automatic playback started!
+    //     })
+    //     .catch(function (error) {
+    //       // Automatic playback failed.
+    //       // Show a UI element to let the user manually start playback.
+    //     });
+    // }
   } else {
     video.pause();
-    playButton.style.display = '';
-    pauseButton.style.display = 'none';
+    playButton.style.display = "";
+    pauseButton.style.display = "none";
   }
 };
-
 
 const toggleMute = () => {
   video.muted = !video.muted;
   if (video.muted) {
-    fullVolumeButton.style.display = 'none';
-    mutedButton.style.display = '';
+    fullVolumeButton.style.display = "none";
+    mutedButton.style.display = "block";
   } else {
-    fullVolumeButton.style.display = '';
-    mutedButton.style.display = 'none';
+    fullVolumeButton.style.display = "block";
+    mutedButton.style.display = "none";
   }
 };
 
@@ -107,38 +131,38 @@ const toggleFullScreen = () => {
   }
 };
 
-document.addEventListener('fullscreenchange', () => {
+document.addEventListener("fullscreenchange", () => {
   if (!document.fullscreenElement || !document.webkitCurrentFullScreenElement) {
-    maximizeButton.style.display = '';
-    minimizeButton.style.display = 'none';
+    maximizeButton.style.display = "";
+    minimizeButton.style.display = "none";
   } else {
-    maximizeButton.style.display = 'none';
-    minimizeButton.style.display = '';
+    maximizeButton.style.display = "none";
+    minimizeButton.style.display = "";
   }
 });
 
-document.addEventListener('keyup', (event) => {
-  if (event.code === 'Space') {
+document.addEventListener("keyup", (event) => {
+  if (event.code === "Space") {
     playPause();
   }
 
-  if (event.code === 'KeyM') {
+  if (event.code === "KeyM") {
     toggleMute();
   }
 
-  if (event.code === 'KeyF') {
+  if (event.code === "KeyF") {
     toggleFullScreen();
   }
 
   displayControls();
 });
 
-document.addEventListener('mousemove', () => {
+document.addEventListener("mousemove", () => {
   displayControls();
 });
 
-video.addEventListener('timeupdate', () => {
-  watchedBar.style.width = ((video.currentTime / video.duration) * 100) + '%';
+video.addEventListener("timeupdate", () => {
+  watchedBar.style.width = (video.currentTime / video.duration) * 100 + "%";
   // TODO: calculate hours as well...
   const totalSecondsRemaining = video.duration - video.currentTime;
   // THANK YOU: BEGANOVICH
@@ -147,105 +171,121 @@ video.addEventListener('timeupdate', () => {
   let hours = null;
 
   if (totalSecondsRemaining >= 3600) {
-    hours = (time.getHours().toString()).padStart('2', '0');
+    hours = time.getHours().toString().padStart("2", "0");
   }
 
-  let minutes = (time.getMinutes().toString()).padStart('2', '0');
-  let seconds = (time.getSeconds().toString()).padStart('2', '0');
+  let minutes = time.getMinutes().toString().padStart("2", "0");
+  let seconds = time.getSeconds().toString().padStart("2", "0");
 
-  timeLeft.textContent = `${hours ? hours : '00'}:${minutes}:${seconds}`;
+  timeLeft.textContent = `${hours ? hours : "00"}:${minutes}:${seconds}`;
 });
 
-progressBar.addEventListener('click', (event) => {
-  const pos = (event.pageX - (progressBar.offsetLeft + progressBar.offsetParent.offsetLeft)) / progressBar.offsetWidth;
+progressBar.addEventListener("click", (event) => {
+  const pos =
+    (event.pageX -
+      (progressBar.offsetLeft + progressBar.offsetParent.offsetLeft)) /
+    progressBar.offsetWidth;
   video.currentTime = pos * video.duration;
 });
 
-playPauseButton.addEventListener('click', playPause);
+playPauseButton.addEventListener("click", playPause);
 
-rewindButton.addEventListener('click', () => {
+rewindButton.addEventListener("click", () => {
   video.currentTime -= 10;
 });
 
-fastForwardButton.addEventListener('click', () => {
+fastForwardButton.addEventListener("click", () => {
   video.currentTime += 10;
 });
 
-volumeButton.addEventListener('click', toggleMute);
+volumeButton.addEventListener("click", toggleMute);
 
-fullScreenButton.addEventListener('click', toggleFullScreen);
+fullScreenButton.addEventListener("click", toggleFullScreen);
 
 const setPlaybackSpeed = (value) => {
-  const prevActivceButton = document.getElementsByClassName('playback-item-active')[0];
+  const prevActivceButton = document.getElementsByClassName(
+    "playback-item-active"
+  )[0];
   if (prevActivceButton) {
-    prevActivceButton.classList.remove('playback-item-active');
+    prevActivceButton.classList.remove("playback-item-active");
   }
 
-  const prevActivceLabel = document.getElementsByClassName('speed-label-active')[0];
+  const prevActivceLabel =
+    document.getElementsByClassName("speed-label-active")[0];
   if (prevActivceLabel) {
-    prevActivceLabel.classList.remove('speed-label-active');
+    prevActivceLabel.classList.remove("speed-label-active");
   }
 
+  if (value === ".5") {
+    const playbackButton =
+      document.getElementsByClassName("playback-button")[0];
+    playbackButton.classList.add("playback-item-active");
+    video.playbackRate = 0.5;
 
-  if (value === '.5') {
-    const playbackButton = document.getElementsByClassName('playback-button')[0];
-    playbackButton.classList.add('playback-item-active');
-    video.playbackRate = .5;
-
-    const speedLabel = document.getElementsByClassName('speed-label')[0];
-    speedLabel.classList.add('speed-label-active');
-  }
-  else if (value === '.75') {
-    const playbackButton = document.getElementsByClassName('playback-button')[1];
-    playbackButton.classList.add('playback-item-active');
-    video.playbackRate = .75;
-    const speedLabel = document.getElementsByClassName('speed-label')[1];
-    speedLabel.classList.add('speed-label-active');
-  }
-  else if (value === '1') {
-    const playbackButton = document.getElementsByClassName('playback-button')[2];
-    playbackButton.classList.add('playback-item-active');
+    const speedLabel = document.getElementsByClassName("speed-label")[0];
+    speedLabel.classList.add("speed-label-active");
+  } else if (value === ".75") {
+    const playbackButton =
+      document.getElementsByClassName("playback-button")[1];
+    playbackButton.classList.add("playback-item-active");
+    video.playbackRate = 0.75;
+    const speedLabel = document.getElementsByClassName("speed-label")[1];
+    speedLabel.classList.add("speed-label-active");
+  } else if (value === "1") {
+    const playbackButton =
+      document.getElementsByClassName("playback-button")[2];
+    playbackButton.classList.add("playback-item-active");
     video.playbackRate = 1;
-    const speedLabel = document.getElementsByClassName('speed-label')[2];
-    speedLabel.classList.add('speed-label-active');
-  }
-  else if (value === '1.25') {
-    const playbackButton = document.getElementsByClassName('playback-button')[3];
-    playbackButton.classList.add('playback-item-active');
+    const speedLabel = document.getElementsByClassName("speed-label")[2];
+    speedLabel.classList.add("speed-label-active");
+  } else if (value === "1.25") {
+    const playbackButton =
+      document.getElementsByClassName("playback-button")[3];
+    playbackButton.classList.add("playback-item-active");
     video.playbackRate = 1.25;
-    const speedLabel = document.getElementsByClassName('speed-label')[3];
-    speedLabel.classList.add('speed-label-active');
-  }
-  else if (value === '1.5') {
-    const playbackButton = document.getElementsByClassName('playback-button')[4];
-    playbackButton.classList.add('playback-item-active');
+    const speedLabel = document.getElementsByClassName("speed-label")[3];
+    speedLabel.classList.add("speed-label-active");
+  } else if (value === "1.5") {
+    const playbackButton =
+      document.getElementsByClassName("playback-button")[4];
+    playbackButton.classList.add("playback-item-active");
     video.playbackRate = 4.0;
-    const speedLabel = document.getElementsByClassName('speed-label')[4];
-    speedLabel.classList.add('speed-label-active');
+    const speedLabel = document.getElementsByClassName("speed-label")[4];
+    speedLabel.classList.add("speed-label-active");
   }
-}
+};
 
 const onSpeedButtonClick = () => {
-  const playbackControls = document.querySelector('.playback-controls');
-  playbackControls.style.display = 'block';
-}
+  const playbackControls = document.querySelector(".playback-controls");
+  playbackControls.style.display = "block";
+};
 
 const removePlaybackControls = () => {
-  const playbackControls = document.querySelector('.playback-controls');
-  playbackControls.style.display = 'none';
-}
+  const playbackControls = document.querySelector(".playback-controls");
+  playbackControls.style.display = "none";
+};
 
 const onVolumeChange = (value) => {
   video.volume = +value / 100;
-}
+};
 
-const volumeInput = document.querySelector('.slider');
+const volumeInput = document.querySelector(".slider");
 
 const showVolumeControl = () => {
   volumeInput.style.opacity = 1;
-}
+};
 
 const removeVolumeControl = () => {
   volumeInput.style.opacity = 0;
+};
 
-}
+const goBack = () => {
+  videoContainer.style.opacity = 0;
+  setTimeout(() => {
+    videoContainer.style.display = "none";
+  }, 750);
+
+  const poster = document.querySelector(".poster-container");
+  poster.style.opacity = "1";
+  poster.style.display = "flex";
+};
